57e1efdd803023dfcf28dc2dfa42c239b4675518
raphaelsmuniz <raphaelnsx@gmail.com>
2026-04-21 21:33:26 -0300

    inicio da execucao do exercicio: abaixo da media

---
+++ src/secao10/Vetor9_abaixo_da_media.java
@@ -0,0 +1,19 @@
+package secao10;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Vetor9_abaixo_da_media {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner scan = new Scanner(System.in);
+		
+		
+		
+		scan.close();
+
+	}
+
+}
